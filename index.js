9723594fd65124e7ebbfe0c0e7e33a900b4c6df0
Demetrios Kalcassinas <disinas@icloud.com>
2022-02-04 16:12:23 +0200

    add package: log-symbols for custom styled CLI alerts
    As about the UX of nodeJS CLI use of npmjs package  log-symbols  give styling at the CLI screen for every computer platform it would be run.

--- index.js
+++ index.js
@@ -2,12 +2,18 @@
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
 const chalk = require('chalk');
-const log = console.log; 
 const dim = chalk.dim;
 const italic = chalk.italic;
 const twitterClr = chalk.hex(`#1da1f2`).bold.inverse;
 const githubClr = chalk.hex(`#6cc644`).bold.inverse;
 const purple = chalk.hex(`#6937FF`).bold.inverse;
+
+// Alerts.
+const sym = require('log-symbols');
+const success = chalk.green.inverse;
+const info = chalk.blue.inverse;
+const warning = chalk.keyword(`orange`).inverse;
+const error = chalk.red.bold.inverse;
 
 welcome({
 	title: `Demetrios Kalcassinas`,
@@ -20,11 +26,21 @@ welcome({
 	clear: true,
 });
 
-log(`${italic(
+console.log(`${italic(
 	`Utilised React as a Front-End Web Developer, building stunning user experiences for the Web with the evolving technology.`
 )}
 
 ${twitterClr(` Twitter `)} ${dim(`https://twitter.com/dikalcas`)}
 ${githubClr(` GitHub `)}  ${dim(`https://github.com/desinas`)}
 ${purple(` Blog `)}    ${dim(`https://linkedin.com/in/desinas`)}
+`);
+
+console.log(`
+${success(` SUCCESS `)} Thank for using this CLI. ${sym.success}
+
+${info(` INFO `)} I am a Front-End Web Developer. ${sym.info}
+
+${warning(` WARNING `)} This is a warning message. ${sym.warning}
+
+${error(` ERROR `)} I'm on vacation. Contact me next week. ${sym.error}
 `);

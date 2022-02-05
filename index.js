f938ce07d25ebb97a5290460f6bca2829a904cb7
Demetrios Kalcassinas <disinas@icloud.com>
2022-02-05 11:14:46 +0200

    add pack: cli-check-node check the node.js version before execution
    As about the Node CLI Error handling when they try to run it on a lower version Node, where CLI is not supported. To do that we will use  cli-check-node  so, we can define a particular version of node that is supported in the CLI.

--- index.js
+++ index.js
@@ -2,18 +2,14 @@
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
 const chalk = require('chalk');
+const alert = require('cli-alerts');
+const checkNode = require('cli-check-node');
+
 const dim = chalk.dim;
 const italic = chalk.italic;
 const twitterClr = chalk.hex(`#1da1f2`).bold.inverse;
 const githubClr = chalk.hex(`#6cc644`).bold.inverse;
 const purple = chalk.hex(`#6937FF`).bold.inverse;
-
-// Alerts.
-const sym = require('log-symbols');
-const success = chalk.green.inverse;
-const info = chalk.blue.inverse;
-const warning = chalk.keyword(`orange`).inverse;
-const error = chalk.red.bold.inverse;
 
 welcome({
 	title: `Demetrios Kalcassinas`,
@@ -26,6 +22,8 @@ welcome({
 	clear: true,
 });
 
+checkNode('10');
+
 console.log(`${italic(
 	`Utilised React as a Front-End Web Developer, building stunning user experiences for the Web with the evolving technology.`
 )}
@@ -35,12 +33,4 @@ ${githubClr(` GitHub `)}  ${dim(`https://github.com/desinas`)}
 ${purple(` Blog `)}    ${dim(`https://linkedin.com/in/desinas`)}
 `);
 
-console.log(`
-${success(` SUCCESS `)} Thank for using this CLI. ${sym.success}
-
-${info(` INFO `)} I am a Front-End Web Developer. ${sym.info}
-
-${warning(` WARNING `)} This is a warning message. ${sym.warning}
-
-${error(` ERROR `)} I'm on vacation. Contact me next week. ${sym.error}
-`);
+alert({ type: 'info', msg: `Check out! I am ready to help you build your app.` });

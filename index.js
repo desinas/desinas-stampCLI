aa3efec3af57379dd7e7f7c1a0ecf3dbf652fbfa
Demetrios Kalcassinas <disinas@icloud.com>
2022-02-07 23:42:46 +0200

    Node.js process.argv arguments and parsing CLI arguments
    Node process.argv returns an array containing the command line arguments passed to a nodejs process
    On executing  console.log(process.argv)  inside   index.js  gives the nodejs executable path, also the path the file that is running this process, the given flag and the input for this flag. Args starts from the third item of array.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const alert = require('cli-alerts');
 const handleError = require('cli-handle-error');
 
 const init = require('./utils/init');
+const args = process.argv.slice(2);
 
 const dim = chalk.dim;
 const italic = chalk.italic;
@@ -11,24 +12,38 @@ const twitterClr = chalk.hex(`#1da1f2`).bold.inverse;
 const githubClr = chalk.hex(`#6cc644`).bold.inverse;
 const purple = chalk.hex(`#6937FF`).bold.inverse;
 
+const socialInfo = `
+${twitterClr(` Twitter `)} ${dim(`https://twitter.com/dikalcas`)}
+${githubClr(` GitHub `)}  ${dim(`https://github.com/desinas`)}
+${purple(` Blog `)}    ${dim(`https://linkedin.com/in/desinas`)}
+`;
+
+const adInfo = alert({ type: 'info', msg: `Check out! I am ready to help you build your app.` });
+
+const printSocial = args.indexOf('--no-social') === -1;
+const social = printSocial ? socialInfo : ``;
+
+const printAd = args.indexOf('--no-ad') === -1;
+const ad = printAd ? adInfo : ``;
+
 (async () => {
 	init();
 
-	console.log(`${italic(
-		`Utilised React as a Front-End Web Developer, building stunning user experiences for the Web with the evolving technology.`
-	)}
+	console.log(
+		`${italic(
+			`Utilised React as a Front-End Web Developer, building stunning user experiences for the Web with the evolving technology.
+			`
+		)}
 
-	${twitterClr(` Twitter `)} ${dim(`https://twitter.com/dikalcas`)}
-	${githubClr(` GitHub `)}  ${dim(`https://github.com/desinas`)}
-	${purple(` Blog `)}    ${dim(`https://linkedin.com/in/desinas`)}
-	`);
+		${social}
+		${ad}
+		`
+	);
 
-	alert({ type: 'info', msg: `Check out! I am ready to help you build your app.` });
-
-	let err;
-	err = false;
-	// err = new Error(`Blog API`); // OR Error can be a real error.
-	if (err) {
-		handleError(`Blog API is down.`, err, true, false);
-	}
+	// let err;
+	// err = false;
+	// // err = new Error(`Blog API`); // OR Error can be a real error.
+	// if (err) {
+	// 	handleError(`Blog API is down.`, err, true, false);
+	// }
 })();
